c4f84556a6536d6bd6d73235b39ccceddceb0469
Miguel <milito_vanetta@hotmail.com>
2020-06-02 16:01:43 -0300

    Clase 'Personaje' creada

---
+++ ControlDeProgreso2/Clases/clases.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Clases
+{
+    enum maximos
+    {
+        VelocidadMax = 10,
+        DestrezaMax = 5,
+        FuerzaMax = 10,
+        NivelMax = 10,
+        ArmaduraMax = 10,
+    }
+
+    public class Personaje
+    {
+        string tipo;
+        string nombre;
+        string apodo;
+        DateTime fechaNacimiento;
+        private int edad;
+        int salud;
+        int velocidad;
+        int destreza;
+        int fuerza;
+        int nivel;
+        int armadura;
+    }
+}
