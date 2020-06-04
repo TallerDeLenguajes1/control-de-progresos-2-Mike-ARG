c85e75d0ea6a4401531b8968de75b7b52bcd99a0
Miguel <milito_vanetta@hotmail.com>
2020-06-04 16:21:07 -0300

    Creación de personaje

--- ControlDeProgreso2/Clases/clases.cs
+++ ControlDeProgreso2/Clases/clases.cs
@@ -9,6 +9,7 @@ namespace Clases
         FuerzaMax = 10,
         NivelMax = 10,
         ArmaduraMax = 10,
+        SaludMax = 100,
     }
 
     public class Personaje
@@ -36,5 +37,33 @@ namespace Clases
         public int Fuerza { get => fuerza; set => fuerza = value; }
         public int Nivel { get => nivel; set => nivel = value; }
         public int Armadura { get => armadura; set => armadura = value; }
+    
+    
+        public Personaje CargarDatos(Personaje Personaje)
+        {
+            string[] tiposPersonajes = { "Duende", "Caballero", "Mago", "Troll", "Humano" };
+            Random rand = new Random();
+
+            Personaje.Tipo = tiposPersonajes[rand.Next(tiposPersonajes.Length)]; //Selecciona un tipo de personaje al azar
+
+            Console.WriteLine("Ingrese el nombre del personaje: ");
+            Personaje.Nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese el apodo del personaje: ");
+            Personaje.Apodo = Console.ReadLine();
+            Console.WriteLine("Ingrese la fecha de nacimiento del personaje: ");
+            Personaje.FechaNacimiento = Convert.ToDateTime(Console.ReadLine());
+            Personaje.Edad = DateTime.Now.Year - Personaje.FechaNacimiento.Year;
+            Personaje.Salud = Convert.ToInt32(Maximos.SaludMax);
+            Personaje.Velocidad = rand.Next(Convert.ToInt32(Maximos.DestrezaMax));
+            Personaje.Destreza = rand.Next(Convert.ToInt32(Maximos.DestrezaMax));
+            Personaje.Fuerza = rand.Next(Convert.ToInt32(Maximos.FuerzaMax));
+            Personaje.Nivel = rand.Next(Convert.ToInt32(Maximos.NivelMax));
+            Personaje.Armadura = rand.Next(Convert.ToInt32(Maximos.ArmaduraMax));
+
+
+            return Personaje;
+        }
+    
+    
     }
 }
--- ControlDeProgreso2/ControlDeProgreso2/Program.cs
+++ ControlDeProgreso2/ControlDeProgreso2/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Clases;
+using System;
 
 namespace ControlDeProgreso2
 {
@@ -6,7 +7,7 @@ namespace ControlDeProgreso2
     {
         static void Main(string[] args)
         {
-            
+            Personaje MiPersonaje = new Personaje();
             Random valoresAleatorios = new Random();
             valoresAleatorios.Next(100);
             Console.WriteLine("Hello World!");
